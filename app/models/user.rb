2acf069969b5b7f616e6b387b78078666a878c9c
Clement <clementlecuillier@hotmail.fr>
2018-02-05 11:30:30 +0100

    password_digest migration done

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :username, presence: true,
+                       uniqueness: true
+
+
+  Email_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: {with: Email_REGEX},
+                    uniqueness: {case_sensitive: false}
+end
