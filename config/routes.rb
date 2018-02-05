078f366a11895544ff921990beb6e4adbf922382
Clement <clementlecuillier@hotmail.fr>
2018-02-05 18:40:25 +0100

    toujours le bordel

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :sessions
+
   root  'static_pages#home'
   get   '/home',   to: 'static_pages#home'
   get   '/signup', to: 'users#new'
   post  '/signup', to: 'user#create'
-  get   '/secret', to: 'static_pages#secret'
+  get   '/secret', to: 'static_pages#secret',   as: 'secret'
   get   '/login',  to: 'sessions#new'
   post  '/login',  to: 'sessions#create'
-  delete'/login',  to: 'sessions#destroy'
-  resources :users
+  delete'/login',  to: 'sessions#destroy',      as: 'logout'
 end
