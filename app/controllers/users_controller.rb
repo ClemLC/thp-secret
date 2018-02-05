b3cc59ef571ff5a98c024ec1d69c8825e6f2af37
Clement <clementlecuillier@hotmail.fr>
2018-02-05 15:56:41 +0100

    bootstrap done ca a de la gueule

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user)
+      log_in @user
+      flash[:success] = "Hey ! Bienvenu sur THP Secret !"
+      redirect_to @user
     else
       render 'new'
     end
@@ -15,7 +17,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.require(:user).permit(:username, :email, :password_digest, :password_digest_confirmation)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root  'static_pages#home'
+  get   '/signup', to: 'users#new'
+  get   '/login',  to: 'sessions#new'
+  post  '/login',  to: 'sessions#create'
+  delete'/login',  to: 'sessions#destroy'
   resources :users
-  root 'static_pages#home'
 end
