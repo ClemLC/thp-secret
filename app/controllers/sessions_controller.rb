85f28165d03c45e81c4f3818fde53b0e292dad71
Clement <clementlecuillier@hotmail.fr>
2018-02-06 15:28:35 +0100

    remember me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
       if
         user && user.authenticate(params[:session][:password])
         log_in user
+        remember user
         redirect_to root_path
       else
         flash.now[:danger] = 'email ou mot de passe invalide(s)'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
