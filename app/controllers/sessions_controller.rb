564193120653bbb4e9b91951346dfaefd35ad616
Clement <clementlecuillier@hotmail.fr>
2018-02-06 16:53:00 +0100

    checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    ?user = User.find_by(email: params[:session][:email].downcase)
       if
-        user && user.authenticate(params[:session][:password])
-        log_in user
-        remember user
+        ?user && user.authenticate(params[:session][:password])
+        log_in ?user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_to root_path
       else
         flash.now[:danger] = 'email ou mot de passe invalide(s)'
